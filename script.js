ce4fa433e98351f36c270ecef13575ab5934a708
Robert Moel <robert.moel@rvmconsulting1.com>
2020-02-05 18:26:39 -0500

    Made City prepender work with clear input, and lists.  Fixed search button shape

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 var inTxt="";
 var iconPic = "";
 
+function addCity(cityName){
+    newLi = $("<li>").text(cityName).attr("class","list-group-item");
+    $("#city").prepend(newLi);
+}
+
+function clrInput(){
+    $("#txtbx").val("");
+}
+
+function clrCity(){
+    $("#city").empty();
+}
+
+clrCity();
+
 $(".btn").on("click", function(e){
     e.preventDefault();
     inCity = $.trim($("#txtbx").val());
     console.log(inTxt);
     queryBlnk = "https:\\api.openweathermap.org/data/2.5/weather?q=";
     query = queryBlnk+inCity+"&APPID=7872f4a78b77312be971aa3219f71aef";
+    addCity(inCity);
+    clrInput();
     
-    $.ajax(
+/*     $.ajax(
         {url:query,
         type:"GET"
         }).then(function(resp){
@@ -16,6 +33,6 @@ $(".btn").on("click", function(e){
             console.log(resp.weather[0]);
             iconPic = "http://openweathermap.org/img/wn/"+resp.weather[0].icon+"@2x.png";
             console.log(iconPic);
-        });
+        }); */
 
 })
